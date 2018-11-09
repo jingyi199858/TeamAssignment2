3062126b3d87847f173cc04771f95d941d219996
blackjack <jingdakai@gmail.com>
2018-11-09 18:57:14 -0500

    update

--- js/script.js
+++ js/script.js
@@ -16,3 +16,18 @@ function myPrivateConverterMany() {
         getExchangeRatesDiv('xxrates',xxv,xxf,xxt,'true');
     }
 }
+
+$(function() {
+    $('#profiletabs ul li a').on('click', function(e) {
+        e.preventDefault();
+        var newcontent = $(this).attr('href');
+        $('#profiletabs ul li a').removeClass('sel');
+        $(this).addClass('sel');
+        $('#content section').each(function() {
+            if (!$(this).hasClass('hidden')) {
+                $(this).addClass('hidden');
+            }
+        });
+        $(newcontent).removeClass('hidden');
+    });
+});
